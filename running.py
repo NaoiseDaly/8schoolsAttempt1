77d884220e757cdb129fd65265a19a0fd5009997
Naoise <naoisedaly101@gmail.com>
2024-11-25 11:19:04 +0000

    show empirical dist of mu given tau = 5

--- running.py
+++ running.py
@@ -1,5 +1,15 @@
 from functions import *
 import logging 
+import matplotlib.pyplot as plt
 logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
 
+tau = 5
+print(tau)
+a, b = get_total_precision_and_precision_weighted_average(tau)
+print(a, "\n", b)
+
+mus = [mu_given_tau(5) for _ in range(200)]
+print(np.mean(mus), np.var(mus))
+plt.hist(mus, bins =20)
+plt.show()
