b01bce30f62d176c84270970241f3e8b2439ef59
Naoise <naoisedaly101@gmail.com>
2024-11-25 19:49:34 +0000

    add hist of mcmc chain

--- running.py
+++ running.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
 
-np.random.seed(457)
+np.random.seed(4242)
 
-tau_chain1 = tau_MCMC(5, 10 )
+tau_chain1 = tau_MCMC(.01, 10**5)
 
 simply_plot_the_chain(tau_chain1)
 
-# plt.hist(tau_chain1, bins = 20)
-# plt.show()
+emp_mean_tau, emp_sd_tau = np.mean(tau_chain1), np.std(tau_chain1)
+plt.hist(tau_chain1, bins = 30)
+plt.title(f"x bar {emp_mean_tau:.1f} s {emp_sd_tau:.1f}")
+plt.show()
