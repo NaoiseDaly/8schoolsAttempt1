2b983d0ee184fb305b22ad8fb04c1c2d7f9dfd4c
Naoise <naoisedaly101@gmail.com>
2024-11-26 10:20:08 +0000

    show posterior quantiles across schools

--- running.py
+++ running.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
 
-np.random.seed(4242)
+np.random.seed(42421)
 
-tau_chain1 = tau_MCMC(5, 1000)
+tau_chain1 = tau_MCMC(5, 10000)
 
 #visualise tau
 simply_plot_the_chain(tau_chain1, title = "tau MCMC")
@@ -16,19 +16,21 @@ plt.suptitle("histogram of tau")
 plt.title(f"x bar {emp_mean_tau:.1f},  s {emp_sd_tau:.1f}")
 plt.show()
 
-effect_in_A = []
-max_effect = []
-for tau in tau_chain1:
-    mu = mu_given_tau(tau)
 
-    theta = theta_given_hyperparams(mu, tau, DATA["yBar.j"], DATA["sigma.j"])
+effects = np.ndarray(shape = (len(tau_chain1), len("ABCDEFGH")) )
+for i, tau in enumerate(tau_chain1):
 
-    effect_in_A.append(theta[0]) # look we just know A will be the first
-    max_effect.append(max(theta))
+    effects[i] = theta_given_hyperparams(mu_given_tau(tau), tau, DATA["yBar.j"], DATA["sigma.j"])
+
+effects = pd.DataFrame(effects).rename(columns = dict(  enumerate("ABCDEFGH") ) )
 
 fig , ( ax1, ax2) = plt.subplots(1,2)
-ax1.hist(effect_in_A, bins = 30)
+ax1.hist(effects["A"], bins = 30)
 ax1.set_title("effect in school A")
-ax2.hist(max_effect, bins = 30)
+ax2.hist(effects.max(axis =1), bins = 30)
 ax2.set_title("largest effect")
 plt.show()
+print("posterior quantiles")
+print(
+    effects.quantile(q = [.025, .25, .5, .75, .975]).round(0).astype(np.int32)
+)
